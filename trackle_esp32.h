ac9b99256364a87f11f0f21dafee06e6d1f59a2c
Federico Bombardieri <bombardieri.federico@gmail.com>
2023-07-19 12:59:14 +0200

    chore: updated lib for logs on setters before init

--- trackle_esp32.h
+++ trackle_esp32.h
@@ -20,7 +20,7 @@
 #ifndef TRACKLE_ESP32_H
 #define TRACKLE_ESP32_H
 
-#define TRACKLE_ESP32_VERSION "3.0.0"
+#define TRACKLE_ESP32_VERSION "3.0.1"
 
 #include "trackle_interface.h"
 #include "trackle_utils.h"
