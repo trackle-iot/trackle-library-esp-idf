17054ccd85244c1736175a9601e226a9475c4435
Federico Bombardieri <bombardieri.federico@gmail.com>
2024-02-09 10:18:34 +0100

    chore: increased version in main header

--- trackle_esp32.h
+++ trackle_esp32.h
@@ -20,7 +20,7 @@
 #ifndef TRACKLE_ESP32_H
 #define TRACKLE_ESP32_H
 
-#define TRACKLE_ESP32_VERSION "4.0.0"
+#define TRACKLE_ESP32_VERSION "4.1.0"
 
 #include "trackle_interface.h"
 #include "trackle_utils.h"
